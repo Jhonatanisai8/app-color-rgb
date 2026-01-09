9b5b6fc864e6dbc3c3a1076b45134c46e99a9378
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-08 23:10:55 -0500

    obtiendos los elementos mediante js

---
+++ app.js
@@ -0,0 +1,7 @@
+const inputR = document.getElementById("rojo");
+const inputG = document.getElementById("verde");
+const inputB = document.getElementById("azul");
+
+const textoRojo = document.getElementById("texto-rojo");
+const textoVerde = document.getElementById("texto-verde");
+const textoAzul = document.getElementById("texto-azul");
