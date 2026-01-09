56f898e42e2684ec7de1985b46bf94e5ca31107b
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-08 23:13:36 -0500

    mostrar valores iniciales

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const inputB = document.getElementById("azul");
 const textoRojo = document.getElementById("texto-rojo");
 const textoVerde = document.getElementById("texto-verde");
 const textoAzul = document.getElementById("texto-azul");
+
+let rojo = inputR.value;
+let verde = inputG.value;
+let azul = inputB.value;
+
+textoRojo.innerText = rojo;
+textoVerde.innerText = verde;
+textoAzul.innerText = azul;
+
